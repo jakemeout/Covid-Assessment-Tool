67b3776c931a87f8a6ab92d959d5f97adc12c9c0
dukhyunko93 <dukhyunko93@gmail.com>
2020-09-02 16:19:38 -0400

    migration

---
+++ db/migrate/20200902185821_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :dob
+      t.string :user_name
+      t.string :password
+      t.string :current_location
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20200902185939_create_locations.rb
@@ -0,0 +1,13 @@
+class CreateLocations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :locations do |t|
+      t.string :country
+      t.string :state
+      t.string :city
+      t.string :county
+      t.integer :fips
+
+      t.timestamps
+    end
+  end
+end
