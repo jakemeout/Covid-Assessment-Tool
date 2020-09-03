c5f59eab84e2fe75e0a8a8ef159eb74a3f2cbba3
jakemeout <jacobosity@gmail.com>
2020-09-03 09:04:51 -0400

    updated csv func

--- app/models/nytuscounty.rb
+++ app/models/nytuscounty.rb
@@ -1,14 +1,30 @@
 require 'csv'
 require 'net/http'
+require 'active_record'
+require 'activerecord-import'
 
 class Nytuscounty < ApplicationRecord
- 
-    def self.getdata
+
+    def self.get_data_first
         uri = URI.parse('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv')
         res = Net::HTTP.get(uri)
-        something = CSV.parse(res)
-        while i < something.length do 
-        end
+        val = CSV.parse(res)
+        columns = [:date, :county, :state, :fips, :cases, :deaths]
+        values = val
+        
+        Nytuscounty.import columns, values, validate: false, header: true
         
     end
+
+    # def self.update_data
+    #     uri = URI.parse('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv')
+    #     res = Net::HTTP.get(uri)
+    #     val = CSV.parse(res)
+    #     #something slower that uses update
+        
+    #     # Nytuscounty.update
+        
+    # end
 end
+
+#  unique_by: :fips
