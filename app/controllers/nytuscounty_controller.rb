c431eb8f63c7e2cddc5fda20f25112b323035b8a
jakemeout <jacobosity@gmail.com>
2020-09-08 17:28:44 -0400

    nickname works

--- app/controllers/nytuscounty_controller.rb
+++ app/controllers/nytuscounty_controller.rb
@@ -4,11 +4,9 @@ class NytuscountyController < ApplicationController
     def getdata
         @data = []
         @fips = params[:fips]
-
-        (Date.today-6..Date.today).each do |d|
-            @data.push(Nytuscounty.where(date: d.strftime("%Y-%d-%m"), fips: @fips))
+        (Date.today-7..Date.today).each do |d|
+            @data.push(Nytuscounty.where(date: d.strftime("%Y-%m-%d"), fips: @fips))
         end
-        byebug
         render json: { data: @data }, status: :accepted
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :users, only: [:create]
   post '/login', to: 'auth#create'
   get '/profile', to: 'users#profile'
-  get '/nytdata',  to: 'nytuscounty#getdata'
+  get '/nytdata/:fips',  to: 'nytuscounty#getdata'
 end
