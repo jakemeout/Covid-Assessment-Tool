6bf32eb0fb222e28ba7f40bdda24b58eba3495f1
dukhyunko93 <dukhyunko93@gmail.com>
2020-10-14 11:25:26 -0400

    new key

--- src/components/MapStyle.js
+++ src/components/MapStyle.js
@@ -11,7 +11,7 @@ export default [
         ]
     },
     {
-        "featureType": "all",
+        "featureType": "all",    
         "elementType": "geometry.stroke",
         "stylers": [
             {
