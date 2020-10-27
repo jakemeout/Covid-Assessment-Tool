63ed2f55bddd703b0f8ce04c98f770eb7368862e
dukhyunko93 <dukhyunko93@gmail.com>
2020-10-27 13:12:41 -0400

    updated sign up form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import { Redirect } from 'react-router'
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 export default class SignUp extends Component {
     
@@ -70,53 +75,116 @@ export default class SignUp extends Component {
         }
 
         return (
-            <div className="signup">
-                    <form onSubmit={this.submitHandler}>
-                        <h3>Sign Up</h3>
-
-                        <div className="form-group">
-                            <label>First name</label>
-                            <input type="text" value={this.state.first_name} onChange={(e) => this.onChangeHandler(e)} name="first_name" className="form-control" placeholder="First name" />
-                        </div>
-
-                        <div className="form-group">
-                            <label>Last name</label>
-                            <input type="text" value={this.state.last_name} onChange={(e) => this.onChangeHandler(e)} name="last_name" className="form-control" placeholder="Last name" />
-                        </div>
-
-                        <div className="form-group">
-                            <label>Username</label>
-                            <input type="text" value={this.state.username} onChange={(e) => this.onChangeHandler(e)} name="user_name" className="form-control" placeholder="Enter username" />
-                        </div>
-
-                        <div className="form-group">
-                            <label>Password</label>
-                            <input type="password" value={this.state.password} onChange={(e) => this.onChangeHandler(e)} name="password" className="form-control" placeholder="Enter password" />
-                        </div>
-
-                        <div className="form-group">
-                            <label>Nickname</label>
-                            <input type="text" value={this.state.nickname} onChange={(e) => this.onChangeHandler(e)} name="nickname" className="form-control" placeholder="Enter nickname" />
-                        </div>
-
-                        <div className="form-group">
-                            <label>Date Of Birth</label>
-                            <input type="text" value={this.state.dob} onChange={(e) => this.onChangeHandler(e)} name="dob" className="form-control" placeholder="Enter Date Of Birth" />
-                        </div>
-                        
-                        <div className="form-group">
-                            <label>Current Home Address</label>
-                            <input type="text" value={this.state.current_location} onChange={(e) => this.onChangeHandler(e)} name="current_location" className="form-control" placeholder="Enter your current address" />
-                        </div>
-                        
-                        
-
-                        <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-                        <p className="forgot-password text-right">
-                            Already registered <a href="#">sign in?</a>
-                        </p>
-                    </form>
-                </div>
+            <Container style={{marginTop: "100px"}} component="main" maxWidth="xs">
+            <CssBaseline />
+            <div>
+              <Typography component="h1" variant="h5">
+                Create New Account
+              </Typography>
+              <form style={{width: "100%"}} onSubmit={this.submitHandler} noValidate>
+              <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  value={this.state.first_name} 
+                  onChange={(e) => this.onChangeHandler(e)}
+                  id="first_name"
+                  label="First name"
+                  name="first_name"
+                  autoComplete="firstname"
+                  autoFocus
+                />
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  value={this.state.last_name} 
+                  onChange={(e) => this.onChangeHandler(e)}
+                  id="last_name"
+                  label="Last Name"
+                  name="last_name"
+                  autoComplete="lastname"
+                  autoFocus
+                />
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  value={this.state.user_name} 
+                  onChange={(e) => this.onChangeHandler(e)}
+                  id="user_name"
+                  label="User Name"
+                  name="user_name"
+                  autoComplete="username"
+                  autoFocus
+                />
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  value={this.state.password} 
+                  onChange={(e) => this.onChangeHandler(e)}
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  value={this.state.nickname} 
+                  onChange={(e) => this.onChangeHandler(e)}
+                  id="nickname"
+                  label="Nickname"
+                  name="nickname"
+                  autoComplete="nickname"
+                  autoFocus
+                />                
+                <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                value={this.state.dob} 
+                onChange={(e) => this.onChangeHandler(e)}
+                id="dob"
+                label="Date of Birth"
+                name="dob"
+                autoComplete="dob"
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                value={this.state.current_location} 
+                onChange={(e) => this.onChangeHandler(e)}
+                id="current_location"
+                label="Current Home Address"
+                name="current_location"
+                autoComplete="current_location"
+                autoFocus
+              />
+                <Button
+                  style={{marginTop:"10px"}}
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                >
+                  Sign Up
+                </Button>
+              </form>
+            </div>
+          </Container>
         );
     }
 }
